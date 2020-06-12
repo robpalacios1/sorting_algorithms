57a0306c4cfd8ee6730f305ef76c5c1bb22755d4
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-06-12 18:04:49 -0500

    add comment Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,10 +1,10 @@
 #include "sort.h"
 
 /**
+ * swap - function to swapping values
+ * @a: value one for swap
  *
- *
- *
- *
+ * @b: value two for swap
  */
 
 void swap(int *a, int *b)
@@ -15,16 +15,25 @@ void swap(int *a, int *b)
 	*b = temp;
 }
 
+/**
+ * selection_sort - sorts an array by repeatedly finding the minimum element \
+(considering ascending order) \
+from unsorted part and putting it at the beginning
+ * @array: Array to evaluated
+ *
+ * @size: size of Array
+ */
+
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
 	size_t min_idx = 0, flag = 0;
 
-	for (i = 0; i < size-1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		flag = 0;
 		min_idx = i;
-		for (j = i+1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 			{
