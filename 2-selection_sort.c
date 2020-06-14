682e209428b687dfb9c082410b636c38e6e9d64d
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-06-14 14:16:34 -0500

    add Task 2 Correction

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,19 +12,21 @@ from unsorted part and putting it at the beginning
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
-	size_t min_idx = 0;
+	size_t min_idx = 0, flag = 0;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		flag = 0;
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 			{
 				min_idx = j;
+				flag = 1;
 			}
 		}
-		if (min_idx != i)
+		if (flag)
 		{
 			swap(&array[min_idx], &array[i]);
 			print_array(array, size);
