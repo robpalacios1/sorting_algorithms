ec04b5650d08ce859e95acb8d1e48c5465702d6f
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-06-14 13:57:47 -0500

    add Task 2 Correction

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,32 +4,30 @@
  * selection_sort - sorts an array by repeatedly finding the minimum element \
 (considering ascending order) \
 from unsorted part and putting it at the beginning
- * @array: Array to evaluated
- *
- * @size: size of Array
- */
+* @array: Array to evaluated
+*
+* @size: size of Array
+*/
 
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
-	size_t min_idx = 0, flag = 0;
+	size_t min_idx = 0;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		flag = 0;
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 			{
 				min_idx = j;
-				flag = 1;
 			}
 		}
-		if (flag)
+		if (min_idx != i)
 		{
-		swap(&array[min_idx], &array[i]);
-		print_array(array, size);
+			swap(&array[min_idx], &array[i]);
+			print_array(array, size);
 		}
 	}
 }
